e32fa6bd486e323d3c5e2b18143bc8ab182df5fa
soporte-ml <bluesmeralda4@gmail.com>
2024-10-16 17:33:16 -0600

    Update 1113.js

--- azt/js/1113.js
+++ azt/js/1113.js
@@ -7,7 +7,7 @@ formulario.addEventListener('submit', async(e) =>{
 
 
 try {    
-    const respuesta = await fetch('https://sheet.best/api/sheets/f0f6e966-6ea4-46b1-9d23-dd6302b2f341', {
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/7238af9d-6c55-4fc9-9dbd-3131704e0e98', {
         method: 'POST',
         mode: 'cors',
         headers: {
